1a0bf8cef2b9057381d128991c3449992cc75d25
Cami1090 <camiloforero200145@gmail.com>
2024-10-11 06:05:00 -0500

    controladorPrincipal

--- src/main/java/controlador/ControladorPrincipal.java
+++ src/main/java/controlador/ControladorPrincipal.java
@@ -4,10 +4,46 @@
  */
 package controlador;
 
+import modelo.Locker;
+import util.IList;
+
 /**
  *
  * @author IVAN
  */
 public class ControladorPrincipal {
+    
+    private Locker[][] lockers;
+   
+
+    public ControladorPrincipal() {
+        lockers = new Locker[3][5];
+        initLockers();
+    }
+    
+     private void initLockers() {
+        for (int i = 0; i < lockers.length; i++) {
+            for (int j = 0; j < lockers[i].length; j++) {
+                Locker locker = new Locker(0, null, null);
+                lockers[i][j] = locker;
+            }
+        }
+    }
+     
+     public boolean validarLocker(int id) {
+        for (int i = 0; i < lockers.length; i++) {
+            for (int j = 0; j < lockers[i].length; j++) {
+                if (lockers[i][j].getId() == id) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+     
+     public Locker entregarLocker(int fila, int columna) {
+        return lockers[fila][columna];
+    }
+    
     
 }
