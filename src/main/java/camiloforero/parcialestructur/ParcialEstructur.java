ca2ff94bbac263b5fe0c285ed0d72307badeb10e
Cami1090 <camiloforero200145@gmail.com>
2024-10-11 01:29:49 -0500

    un

---
+++ src/main/java/camiloforero/parcialestructur/ParcialEstructur.java
@@ -0,0 +1,16 @@
+/*
+ * 
+ */
+
+package camiloforero.parcialestructur;
+
+/**
+ *
+ * @author Cami
+ */
+public class ParcialEstructur {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
